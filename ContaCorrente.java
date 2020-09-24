1a7c48358bf53798e54947ccbc9403a0fd7a50f7
Amanda Perrone <amanda_degrande@yahoo.com.br>
2020-09-23 23:44:18 -0300

    first commit

---
+++ ContaCorrente.java
@@ -0,0 +1,85 @@
+/*
+Desenvolva uma aplicação que:
+
+1. Construa a classe ContaCorrente com  4 atributos privados: número conta, saldo, limite do especial, cliente.
+
+2. Regras para a classe ContaCorrente:
+
+    O saldo nunca poderá ser menor do que o valor do limite do especial.
+    O limite do especial deverá ser um valor maior ou igual a zero.
+*/
+
+
+package ac1;
+
+public class ContaCorrente {
+         
+    // Atributos
+    private int numeroConta;
+    private double saldo;
+    private double limiteEspecial;
+    private String nomeCliente;
+
+
+    public double depositar(double valorDeposito) {
+        if (limiteEspecial == limiteEspecialInicial){
+            saldo += valorDeposito;
+            return saldo;
+        } else {
+            double diferencaLimites = limiteEspecialInicial - limiteEspecial;
+            if (valorDeposito >= diferencaLimites) {
+                double depositoParaEspecial = valorDeposito - diferencaLimites;
+                setLimiteEspecial(limiteEspecial + depositoParaEspecial);
+                double depositoParaSaldo = valorDeposito - depositoParaEspecial;
+                saldo += depositoParaSaldo;
+                return saldo;
+            } else {
+                double novoLimite = limiteEspecial + valorDeposito;
+                setLimiteEspecial(novoLimite);
+                return limiteEspecial;
+            }  
+        }
+        
+    }
+
+    // TO STRING, GETTERS AND SETTER
+    @Override
+    public String toString() {
+        return "ContaCorrente [limiteEspecial=" + limiteEspecial + ", nomeCliente=" + nomeCliente + ", numeroConta="
+                + numeroConta + ", saldo=" + saldo + "]";
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimiteEspecial() {
+        return limiteEspecial;
+    }
+
+    public void setLimiteEspecial(double limiteEspecial) {
+        this.limiteEspecial = limiteEspecial;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+        
+
+}
---
+++ TestaContaCorrente.java
@@ -0,0 +1,111 @@
+/*
+3. Crie uma classe chamada TesteContaCorrente com o método main.
+
+4. Dentro do main construir um menu com as seguintes opções.
+
+    -Cadastrar uma nova conta corrente. O número da conta deverá ser único. Todas as contas deverão ser armazenadas em uma lista.
+    -Depositar. Nesta opção o usuário informa o número da conta e o valor do depósito. Se o número da conta existir, fazer o depósito. Se não existir, apresentar uma mensagem de erro.
+    -Saque. Nesta opção o usuário faz um saque informando o número da conta e o valor do saque. Se o número da conta não existir, apresentar uma mensagem de erro, caso contrário fazer o saque respeitando o valor do saldo e o limite da conta.
+    -Ver saldo. Informar o número da conta, e se existir, mostra o saldo. Caso o número da conta não exista, mostrar uma mensagem de erro.
+    -Mostrar o somatório de todos os saldos dos clientes que não usaram o especial.
+    -Sair.
+*/
+
+package ac1;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class TestaContaCorrente {
+    
+    public static void main(String[] args) {
+
+        ArrayList <ContaCorrente> contas = new ArrayList <ContaCorrente>();
+
+        Scanner s = new Scanner(System.in);
+        ContaCorrente cc = new ContaCorrente();
+        int continuar = 1;
+        int i = 1;
+        double verificarLimitePositivo;
+        double limiteEspecialInicial;
+        
+        do{
+            System.out.println("-----------NOBANK-----------");
+            System.out.println("- Digite a opção desejada -");
+            System.out.println(" 1 CADASTRAR UMA NOVA CORRENTE CORRENTE ");
+            System.out.println(" 2 DEPOSITAR ");
+            System.out.println(" 3 SAQUE ");
+            System.out.println(" 4 VER SALDO ");
+            System.out.println(" 5 SOMA DE TODOS OS SALDOS DE CLIENTE QUE NÃO USARAM O ESPECIAL ");
+            System.out.println(" 6 SAIR ");
+            System.out.println("----------------------------");
+            int opcao = Integer.parseInt(s.nextLine());
+    
+            
+            switch(opcao){
+                case 1:
+                    System.out.println("--------------------------1-");
+                    System.out.println(" 1 CADASTRAR UMA NOVA CORRENTE CORRENTE ");
+                    cc.setNumeroConta(i);
+                    System.out.println("Digite seu nome: ");
+                    cc.setNomeCliente(s.nextLine());
+                    cc.setSaldo(0.0);
+                    System.out.println("Digite o limite especial: ");
+                    verificarLimitePositivo = Double.parseDouble(s.nextLine());
+                    while (verificarLimitePositivo < 0) {
+                        System.out.println("Limite deve ser maior ou igual a zero.");
+                        System.out.println("Digite o limite especial: ");
+                        verificarLimitePositivo = Double.parseDouble(s.nextLine());
+                    }
+                    cc.setLimiteEspecial(Double.parseDouble(s.nextLine()));
+                    limiteEspecialInicial = getLimiteEspecial();
+                    System.out.println("Voltar ao MENU?");
+                    continuar = Integer.parseInt(s.nextLine());
+                    i++;
+                    contas.add(cc);
+                    System.out.println("--------------------------1-");
+                    break;
+                case 2:
+                    System.out.println("--------------------------2-");
+                    System.out.println(" 2 DEPOSITAR ");
+                    System.out.println("Informe o número da conta: ");
+                    int a = 3;
+                    cc.setSaldo(0.0);
+                    //pegar o numero e ver se ele tem no array
+                    if (a == 3) {
+                        System.out.println("Informe o valor para depósito: ");
+                        cc.depositar(Double.parseDouble(s.nextLine()));
+                        System.out.println("Saldo atual: " + getSaldo());
+                        System.out.println("Especial: " + getLimiteEspecial());
+                    } else {
+                        System.out.println("Conta inválida");
+                    }
+                    System.out.println("Voltar ao MENU?");
+                    continuar = Integer.parseInt(s.nextLine());
+                    System.out.println("--------------------------2-");
+                    break;
+                case 3:
+                    System.out.println("--------------------------3-");
+                    System.out.println(" 3 SAQUE ");
+
+                    System.out.println("--------------------------3-");
+                    break;
+                case 4:
+                    System.out.println("voce digitou 4");
+                    break;
+                case 5:
+                    System.out.println("voce digitou 5");
+                    break;
+                case 6:
+                    System.out.println("voce digitou 6");
+                    break;
+                default:
+                    System.out.println("OPÇÃO INVÁLIDA");
+                    break;
+            } // case
+
+        } while (continuar == 1);
+
+    }
+    
+}
